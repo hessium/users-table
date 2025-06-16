f17a9423aee396152b6b71e27210e7fefc212b9f
hessium <hessium56@gmail.com>
2025-06-16 20:29:33 +0300

    fix

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
-import { Toaster } from "../shared/ui/sonner/sonner.tsx";
-import { MainLayout } from "../containers/main-layout/main-layout.tsx";
+import { Toaster } from "./shared/ui/sonner/sonner.tsx";
+import { MainLayout } from "./containers/main-layout/main-layout.tsx";
 
 export function App() {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ErrorBoundaryLayout } from "../containers/error-boundary/error-boundary.tsx";
+import { ErrorBoundaryLayout } from "./containers/error-boundary/error-boundary.tsx";
 import { RouterProvider } from "react-router";
 import { router } from "./router.tsx";
-import "../../public/styles/index.css";
+import "../public/styles/index.css";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
